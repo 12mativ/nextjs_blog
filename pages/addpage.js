0bae598d35556bae0e0d80f47dc30f63ef33d4c1
12mativ <ivan_m146@mail.ru>
2023-04-14 12:27:33 +0300

    developer/popup commit 1

--- pages/addpage.js
+++ pages/addpage.js
@@ -3,6 +3,10 @@ export default function AddPage() {
         <>
             <div>Hello</div>
             <div>Add new text with dev branch</div>
+
+            <>
+                <div>developer/popup commit 1</div>
+            </>
         </>
     )
 }
